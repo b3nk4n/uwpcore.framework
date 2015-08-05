90f1ab398933ddf4e2a2cbc960aa10c2bf7c136f
bsautermeister <bsautermeister@live.de>
2015-08-06 00:44:17 +0200

    Added a background task service.

---
+++ Framework/UWPCore.Framework/Tasks/BackgroundTaskService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace UWPCore.Framework.Tasks
+{
+    /// <summary>
+    /// The background task service class.
+    /// </summary>
+    public class BackgroundTaskService : IBackgroundTaskService
+    {
+        public IBackgroundTaskRegistration Register(string taskName, Type taskEntryPoint, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions)
+        {
+            return Register(taskName, taskEntryPoint.FullName, trigger, conditions);
+        }
+
+        public IBackgroundTaskRegistration Register(string taskName, string taskEntryPoint, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions)
+        {
+            // check for existing registration
+            var registration = GetRegistration(taskName);
+            if (registration != null)
+                return registration;
+
+            // create new registration
+            var taskBuilder = new BackgroundTaskBuilder();
+            taskBuilder.Name = taskName;
+            taskBuilder.TaskEntryPoint = taskEntryPoint;
+            taskBuilder.SetTrigger(trigger);
+
+            if (conditions != null)
+            {
+                foreach (var condition in conditions)
+                {
+                    taskBuilder.AddCondition(condition);
+                }
+            }
+
+            return taskBuilder.Register();
+        }
+
+        public IBackgroundTaskRegistration GetRegistration(string taskName)
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == taskName)
+                {
+                    return task.Value;
+                }
+            }
+            return null;
+        }
+
+        public bool RegistrationExists(string taskName)
+        {
+            return GetRegistration(taskName) != null;
+        }
+
+        public void Unregister(string taskName)
+        {
+            var registration = GetRegistration(taskName);
+            if (registration != null)
+                registration.Unregister(true);
+        }
+
+        public void UnregisterAll()
+        {
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                task.Value.Unregister(true);
+            }
+        }
+    }
+}
---
+++ Framework/UWPCore.Framework/Tasks/IBackgroundTaskService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+
+namespace UWPCore.Framework.Tasks
+{
+    /// <summary>
+    /// The interface for the background task service.
+    /// </summary>
+    public interface IBackgroundTaskService
+    {
+        /// <summary>
+        /// Registers a background task.
+        /// </summary>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="taskEntryPoint">The entry point class type.</param>
+        /// <param name="trigger">The trigger.</param>
+        /// <param name="conditions">The optional conditions.</param>
+        /// <returns>Returns the background task registration.</returns>
+        IBackgroundTaskRegistration Register(string taskName, Type taskEntryPoint, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions);
+
+        /// <summary>
+        /// Registers a background task.
+        /// </summary>
+        /// <param name="taskName">The task name.</param>
+        /// <param name="taskEntryPoint">The full qualified task entry point class name.</param>
+        /// <param name="trigger">The trigger.</param>
+        /// <param name="conditions">The optional conditions.</param>
+        /// <returns>Returns the background task registration.</returns>
+        IBackgroundTaskRegistration Register(string taskName, string taskEntryPoint, IBackgroundTrigger trigger, params IBackgroundCondition[] conditions);
+
+        /// <summary>
+        /// Gets the background task registration.
+        /// </summary>
+        /// <param name="taskName">The task name.</param>
+        /// <returns>Returns the background task registration or NULL.</returns>
+        IBackgroundTaskRegistration GetRegistration(string taskName);
+
+        /// <summary>
+        /// Checks if a registration already exists.
+        /// </summary>
+        /// <param name="taskName">The task name</param>
+        /// <returns>True when the registration exists, else false.</returns>
+        bool RegistrationExists(string taskName);
+
+        /// <summary>
+        /// Unregisters a background task.
+        /// </summary>
+        /// <param name="taskName">The background task name.</param>
+        void Unregister(string taskName);
+
+        /// <summary>
+        /// Unregisters all background tasks.
+        /// </summary>
+        void UnregisterAll();
+    }
+}
