d8c504452afaa0c3597c75b9a49673631e440527
bsautermeister <bsautermeister@live.de>
2015-08-06 01:43:25 +0200

    Added network info service.

---
+++ Framework/UWPCore.Framework/Networking/INetworkInfoService.cs
@@ -0,0 +1,25 @@
+﻿
+namespace UWPCore.Framework.Networking
+{
+    /// <summary>
+    /// The network information interface to retrive information about the network
+    /// and the devices connectiviy to the internet.
+    /// </summary>
+    public interface INetworkInfoService
+    {
+        /// <summary>
+        /// Gets whether the user has internet connection.
+        /// </summary>
+        bool HasInternet { get; }
+
+        /// <summary>
+        /// Gets whether the user has Wifi connection.
+        /// </summary>
+        bool HasWifi { get; }
+
+        /// <summary>
+        /// Gets whether the user has no data connection.
+        /// </summary>
+        bool HasNoConnection { get; }
+    }
+}
---
+++ Framework/UWPCore.Framework/Networking/NetworkInfoService.cs
@@ -0,0 +1,76 @@
+﻿using Windows.Networking.Connectivity;
+
+namespace UWPCore.Framework.Networking
+{
+    /// <summary>
+    /// The network information class that provides information about the devies connectivity.
+    /// </summary>
+    public class NetworkInfoService : INetworkInfoService
+    {
+        /// <summary>
+        /// Some other type of network interface.
+        /// </summary>
+        public const uint IANA_OTHER = 1;
+
+        /// <summary>
+        /// An Ethernet network interface.
+        /// </summary>
+        public const uint IANA_ETHERNET = 6;
+
+        /// <summary>
+        /// An IEEE 802.11 wireless network interface.
+        /// </summary>
+        public const uint IANA_WLAN = 71;
+
+        public bool HasInternet
+        {
+            get
+            {
+                var connections = NetworkInformation.GetInternetConnectionProfile();
+                return connections != null && connections.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+
+                /*
+                 * In case of en exception:
+                 * http://stackoverflow.com/questions/13625304/check-internet-connection-availability-in-windows-8
+                 *  class ConnectivityUtil
+                    {
+                        internal static bool HasInternetConnection()
+                        {            
+                            var connections = NetworkInformation.GetConnectionProfiles().ToList();
+                            connections.Add(NetworkInformation.GetInternetConnectionProfile());
+
+                            foreach (var connection in connections)
+                            {
+                                if (connection == null)
+                                    continue;
+
+                                if (connection.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess)
+                                    return true;
+                            }
+
+                            return false;
+                        }
+                    }
+                 */
+            }
+        }
+
+        public bool HasWifi
+        {
+            get
+            {
+                var connections = NetworkInformation.GetInternetConnectionProfile();
+                return connections.NetworkAdapter.IanaInterfaceType == IANA_WLAN;
+            }
+        }
+
+        public bool HasNoConnection
+        {
+            get
+            {
+                var connections = NetworkInformation.GetInternetConnectionProfile();
+                return connections != null && connections.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.None;
+            }
+        }
+    }
+}
