b7560c0735110c163acc8e50aef7300b9d91bda9
bsautermeister <bsautermeister@live.de>
2015-08-06 00:47:31 +0200

    Implemented empty background task.

---
+++ Demo/UWPCore.Demo.Tasks/SimpleBackgroundTask.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace UWPCore.Demo.Tasks
+{
+    /// <summary>
+    /// A simple background task to test the functionality.
+    /// </summary>
+    public sealed class SimpleBackgroundTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+
+            await Task.Delay(100);
+
+            deferral.Complete();
+        }
+    }
+}
