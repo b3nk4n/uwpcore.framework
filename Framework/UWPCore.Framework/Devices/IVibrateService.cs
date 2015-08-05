0a8605195c259e58db550128272efab2d2da73ed
bsautermeister <bsautermeister@live.de>
2015-08-06 01:34:39 +0200

    Added vibrate service.

---
+++ Framework/UWPCore.Framework/Devices/IVibrateService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPCore.Framework.Devices
+{
+    /// <summary>
+    /// The service for the vibration controller.
+    /// </summary>
+    public interface IVibrateService
+    {
+        /// <summary>
+        /// Invokes the vibration controller for the given time.
+        /// </summary>
+        /// <param name="seconds">The vibration time in seconds.</param>
+        void Vibrate(double seconds);
+
+        /// <summary>
+        /// Stops the current vibration.
+        /// </summary>
+        void Stop();
+    }
+
+}
---
+++ Framework/UWPCore.Framework/Devices/VibrateService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Windows.Foundation.Metadata;
+using Windows.Phone.Devices.Notification;
+
+namespace UWPCore.Framework.Devices
+{
+    /// <summary>
+    /// The implementation of the vibration service.
+    /// </summary>
+    /// <remarks>
+    /// This service is only available on mobile devices.
+    /// </remarks>
+    public sealed class PhoneVibrateService : IVibrateService
+    {
+        /// <summary>
+        /// The vibration device.
+        /// </summary>
+        private VibrationDevice _virationDevice;
+
+        /// <summary>
+        /// Creates a <see cref="UniversalKit.Framework.Phone.Devices.PhoneVibrateService"/> instance.
+        /// </summary>
+        public PhoneVibrateService()
+        {
+            if (ApiInformation.IsTypePresent("Windows.Phone.Devices.Notification.VibrationDevice"))
+            {
+                _virationDevice = VibrationDevice.GetDefault();
+            }
+        }
+
+        public void Vibrate(double seconds)
+        {
+            if (_virationDevice != null)
+                _virationDevice.Vibrate(TimeSpan.FromSeconds(seconds));
+        }
+
+        public void Stop()
+        {
+            if (_virationDevice != null)
+                _virationDevice.Cancel();
+        }
+    }
+
+}
