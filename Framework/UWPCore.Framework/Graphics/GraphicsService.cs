2dce07ebfe7dfcdca4a0472bea3816343d07dd33
bsautermeister <bsautermeister@live.de>
2015-08-06 01:38:34 +0200

    Added graphics service to render an image from a UIElement.

---
+++ Framework/UWPCore.Framework/Graphics/GraphicsService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace UWPCore.Framework.Graphics
+{
+    /// <summary>
+    /// The graphics service for rendering images at runtime.
+    /// <remarks>
+    /// Inspired by: http://mariusbancila.ro/blog/2013/11/05/render-the-screen-of-a-windows-store-app-to-a-bitmap-in-windows-8-1/
+    /// </remarks>
+    /// </summary>
+    public class GraphicsService : IGraphicsService
+    {
+        public async Task<RenderTargetBitmap> RenderToStreamAsync(UIElement uiElement, IRandomAccessStream stream, Guid bitmapEncoder)
+        {
+            try
+            {
+                var renderTargetBitmap = new RenderTargetBitmap();
+                await renderTargetBitmap.RenderAsync(uiElement);
+
+                var pixels = await renderTargetBitmap.GetPixelsAsync();
+
+                var logicalDpi = DisplayInformation.GetForCurrentView().LogicalDpi;
+                var encoder = await BitmapEncoder.CreateAsync(bitmapEncoder, stream);
+                encoder.SetPixelData(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Ignore,
+                    (uint)renderTargetBitmap.PixelWidth,
+                    (uint)renderTargetBitmap.PixelHeight,
+                    logicalDpi,
+                    logicalDpi,
+                    pixels.ToArray());
+
+                await encoder.FlushAsync();
+
+                return renderTargetBitmap;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the bitmap encoder of the given file extension.
+        /// </summary>
+        /// <param name="fileExtension">The file extension.</param>
+        /// <returns>The corresponding bitmap encoder.</returns>
+        private Guid GetBitmapEncoder(string fileExtension)
+        {
+            Guid encoderId = BitmapEncoder.JpegEncoderId;
+            switch (fileExtension)
+            {
+                case ".bmp":
+                    encoderId = BitmapEncoder.BmpEncoderId;
+                    break;
+                case ".gif":
+                    encoderId = BitmapEncoder.GifEncoderId;
+                    break;
+                case ".png":
+                    encoderId = BitmapEncoder.PngEncoderId;
+                    break;
+                case ".tif":
+                    encoderId = BitmapEncoder.TiffEncoderId;
+                    break;
+            }
+
+            return encoderId;
+        }
+    }
+
+}
---
+++ Framework/UWPCore.Framework/Graphics/IGraphicsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UWPCore.Framework.Graphics
+{
+    public interface IGraphicsService
+    {
+        /// <summary>
+        /// Renders the UIElement to the stream.
+        /// </summary>
+        /// <param name="uiElement">The UIElement to render.</param>
+        /// <param name="stream">The file stream to render to.</param>
+        /// <param name="bitmapEncoder">The bitmap encoder GUID, such as BitmapEncoder.PngEncoderId.</param>
+        /// <returns>The RenderTargetBitmap containing the rendered data.</returns>
+        Task<RenderTargetBitmap> RenderToStreamAsync(UIElement uiElement, IRandomAccessStream stream, Guid bitmapEncoder);
+    }
+}
