c33756811db1d5b16ef1867d1713382ef80e49e0
bsautermeister <bsautermeister@live.de>
2015-08-06 01:10:23 +0200

    Added serialization serivce using default DataContractSerializer.

---
+++ Framework/UWPCore.Framework/Data/DataContractSerializationService.cs
@@ -0,0 +1,117 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace UWPCore.Framework.Data
+{
+    /// <summary>
+    /// Serialization service to serialize and deserialize data using
+    /// <see cref="DataContractSerializer"/> and <see cref="DataContractJsonSerializer"/>.
+    /// </summary>
+    public sealed class DataContractSerializationService : ISerializationService
+    {
+        #region String
+
+        public void Serialize(Stream stream, string data)
+        {
+            var streamWriter = new StreamWriter(stream);
+            streamWriter.Write(data);
+            streamWriter.Flush();
+        }
+
+        public string Deserialize(Stream stream)
+        {
+            var streamReader = new StreamReader(stream);
+            string result = streamReader.ReadToEnd();
+            return result;
+        }
+
+        #endregion
+
+        #region XML
+
+        public void SerializeXML<T>(Stream stream, T data)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            serializer.WriteObject(stream, data);
+        }
+
+        public string SerializeXML<T>(T data)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                SerializeXML<T>(memStream, data);
+
+                memStream.Seek(0, SeekOrigin.Begin);
+                using (var streamReader = new StreamReader(memStream))
+                {
+                    string result = streamReader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+
+        public T DeserializeXML<T>(Stream stream)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            var result = (T)serializer.ReadObject(stream);
+            return result;
+        }
+
+        public T DeserializeXML<T>(string data)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                byte[] encodedData = System.Text.Encoding.UTF8.GetBytes(data);
+                memStream.Write(encodedData, 0, encodedData.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                return DeserializeXML<T>(memStream);
+            }
+        }
+
+        #endregion
+
+        #region JSON
+
+        public void SerializeJson<T>(Stream stream, T data)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            serializer.WriteObject(stream, data);
+        }
+
+        public string SerializeJson<T>(T data)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                SerializeJson<T>(memStream, data);
+
+                memStream.Seek(0, SeekOrigin.Begin);
+                using (var streamReader = new StreamReader(memStream))
+                {
+                    string result = streamReader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+
+        public T DeserializeJson<T>(Stream stream)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            var result = (T)serializer.ReadObject(stream);
+            return result;
+        }
+
+        public T DeserializeJson<T>(string data)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                byte[] encodedData = System.Text.Encoding.UTF8.GetBytes(data);
+                memStream.Write(encodedData, 0, encodedData.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                return DeserializeJson<T>(memStream);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Framework/UWPCore.Framework/Data/ISerializationService.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+
+namespace UWPCore.Framework.Data
+{
+    /// <summary>
+    /// Interface for serialization service.
+    /// </summary>
+    public interface ISerializationService
+    {
+        /// <summary>
+        /// Serialize string to stream as text.
+        /// </summary>
+        /// <param name="stream">Target stream.</param>
+        /// <param name="data">Data to serialize.</param>
+        void Serialize(Stream stream, string data);
+
+        /// <summary>
+        /// Deserialize text from stream to string.
+        /// </summary>
+        /// <param name="stream">Source stream.</param>
+        /// <returns>Data as string.</returns>
+        string Deserialize(Stream stream);
+
+        /// <summary>
+        /// Serialize object to stream as xml.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="stream">Target stream.</param>
+        /// <param name="data">Data to serialize.</param>
+        void SerializeXML<T>(Stream stream, T data);
+
+        /// <summary>
+        /// Serialize object as xml.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="data">Data to serialize.</param>
+        /// <returns>The serialized data or NULL in case of an error.</returns>
+        string SerializeXML<T>(T data);
+
+        /// <summary>
+        /// Deserialize xml from stream to object.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="stream">Source stream.</param>
+        /// <returns>Data as object.</returns>
+        T DeserializeXML<T>(Stream stream);
+
+        /// <summary>
+        /// Deserialize xml to object.
+        /// </summary>
+        /// <typeparam name="T">Type of data.</typeparam>
+        /// <param name="data">Data to serialize.</param>
+        /// <returns>The deserialized object or NULL in case of an error.</returns>
+        T DeserializeXML<T>(string data);
+
+        /// <summary>
+        /// Serialize object to stream as json.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="stream">Target stream.</param>
+        /// <param name="data">Data to serialize.</param>
+        void SerializeJson<T>(Stream stream, T data);
+
+        /// <summary>
+        /// Serialize object as json.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="data">The data to serialize.</param>
+        /// <returns>The serialized data or NULL in case of an error.</returns>
+        string SerializeJson<T>(T data);
+
+        /// <summary>
+        /// Deserialize xml from stream.
+        /// </summary>
+        /// <typeparam name="T">Type of object.</typeparam>
+        /// <param name="stream">Source stream.</param>
+        /// <returns>Data as object.</returns>
+
+        T DeserializeJson<T>(Stream stream);
+
+        /// <summary>
+        /// Deserialize xml to object.
+        /// </summary>
+        /// <typeparam name="T">Type of data.</typeparam>
+        /// <param name="data">The data to deserialize.</param>
+        /// <returns>The deserialized object or NULL in case of an error.</returns>
+        T DeserializeJson<T>(string data);
+    }
+
+}
