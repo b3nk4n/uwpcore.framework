7c71eba857e2e51edf3bc04a01fe9da1b53e9130
bsautermeister <bsautermeister@live.de>
2015-08-06 11:52:03 +0200

    Added unit test for StoredObject.

---
+++ Test/UWPCore.Test/Storage/StoredObjectTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using UWPCore.Framework.Storage;
+
+namespace UWPCore.Test.Storage
+{
+    [TestClass]
+    public class StoredObjectTest
+    {
+        /// <summary>
+        /// The used test key.
+        /// </summary>
+        private const string TEST_KEY = "test_key";
+
+        /// <summary>
+        /// The used default value.
+        /// </summary>
+        private const string DEFAULT_VALUE = "default_value";
+
+        /// <summary>
+        /// The system under test.
+        /// </summary>
+        private StoredObject<string> _storedObject;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _storedObject = new StoredObject<string>(TEST_KEY, DEFAULT_VALUE);
+        }
+
+        [TestMethod]
+        public void TestDefaultValue()
+        {
+            Assert.AreEqual(DEFAULT_VALUE, _storedObject.DefaultValue);
+            Assert.AreEqual(DEFAULT_VALUE, _storedObject.Value);
+        }
+
+        [TestMethod]
+        public void TestWriteAndReadValue()
+        {
+            string data = "data";
+
+            // change data
+            _storedObject.Value = data;
+
+            Assert.AreEqual(data, _storedObject.Value);
+        }
+
+        [TestMethod]
+        public void TestReadKey()
+        {
+            Assert.AreEqual(TEST_KEY, _storedObject.Key);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _storedObject.Delete();
+        }
+    }
+
+}
