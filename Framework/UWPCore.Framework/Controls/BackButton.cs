f814985ae869c3b26c46571964baaeba0c409366
bsautermeister <bsautermeister@live.de>
2015-08-06 12:20:26 +0200

    Added primitive UI controls for page header.

---
+++ Framework/UWPCore.Framework/Controls/BackButton.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace UWPCore.Framework.Controls
+{
+    public sealed class BackButton : Button
+    {
+        public event EventHandler VisibilityChanged;
+
+        public BackButton()
+        {
+            Style = Resources["NavigationBackButtonNormalStyle"] as Style;
+            DefaultStyleKey = typeof(BackButton);
+            Loaded += (s, e) =>
+            {
+                DependencyObject item = this;
+                while (!((item = VisualTreeHelper.GetParent(item)) is Page)) { }
+                Page page = item as Page;
+                Frame = page.Frame;
+                Visibility = CalculateOnCanvasBackVisibility();
+            };
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                Visibility = Visibility.Visible;
+                return;
+            }
+            Click += (s, e) => Frame.GoBack();
+            Window.Current.SizeChanged += (s, arg) => Visibility = CalculateOnCanvasBackVisibility();
+            RegisterPropertyChangedCallback(VisibilityProperty, (s, e) => VisibilityChanged?.Invoke(this, EventArgs.Empty));
+        }
+
+        public Frame Frame { get; private set; }
+
+        private Visibility CalculateOnCanvasBackVisibility()
+        {
+            // by design it is not visible when not applicable
+            var cangoback = Frame.CanGoBack;
+            if (!cangoback)
+                return Visibility.Collapsed;
+
+            // mobile always has a visible back button
+            var mobilefam = ResourceContext.GetForCurrentView().QualifierValues["DeviceFamily"].Equals("Mobile");
+            if (mobilefam)
+                return Visibility.Collapsed;
+
+            // simply don't know what to do with else
+            var desktopfam = ResourceContext.GetForCurrentView().QualifierValues["DeviceFamily"].Equals("Desktop");
+            if (!desktopfam)
+                return Visibility.Collapsed;
+
+            // touch always has a bisible back button
+            var touchmode = UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Touch;
+            if (touchmode)
+                return Visibility.Collapsed;
+
+            // full screen back button is only visible when mouse reveals title (prefered behavior is on-canvas visible)
+            var fullscreen = ApplicationView.GetForCurrentView().IsFullScreen;
+            if (fullscreen)
+                return Visibility.Visible;
+
+            // hide the button if the shell button is visible
+            var optinback = SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility.Equals(AppViewBackButtonVisibility.Visible);
+            if (optinback)
+            {
+                // shell button will not be visible if there is no title bar
+                var hastitle = !CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar;
+                if (hastitle)
+                    return Visibility.Collapsed;
+            }
+
+            // at this point, we show the on-canvas button
+            return Visibility.Visible;
+        }
+    }
+}
---
+++ Framework/UWPCore.Framework/Controls/PageHeader.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPCore.Framework.Controls
+{
+    /// <summary>
+    /// A simple page header class which includes a back and forward button.
+    /// </summary>
+    public sealed partial class PageHeader : UserControl
+    {
+        public PageHeader()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string),
+                typeof(PageHeader), new PropertyMetadata("Page Header"));
+
+        public bool ShowBack
+        {
+            get { return (bool)GetValue(ShowBackProperty); }
+            set { SetValue(ShowBackProperty, value); }
+        }
+        public static readonly DependencyProperty ShowBackProperty =
+            DependencyProperty.Register("ShowBack", typeof(bool),
+                typeof(PageHeader), new PropertyMetadata(true, ShowBackChanged));
+        private static void ShowBackChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var p = d as PageHeader;
+            var g = p.Content as Grid;
+            if ((bool)e.NewValue)
+            {
+                g.ColumnDefinitions[0].Width = GridLength.Auto;
+            }
+            else
+            {
+                g.ColumnDefinitions[0].Width = new GridLength(0);
+            }
+        }
+
+        public bool ShowForward
+        {
+            get { return (bool)GetValue(ShowForwardProperty); }
+            set { SetValue(ShowForwardProperty, value); }
+        }
+        public static readonly DependencyProperty ShowForwardProperty =
+            DependencyProperty.Register("ShowForward", typeof(bool),
+                typeof(PageHeader), new PropertyMetadata(true, ShowForwardChanged));
+        private static void ShowForwardChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var p = d as PageHeader;
+            var g = p.Content as Grid;
+            if ((bool)e.NewValue)
+            {
+                g.ColumnDefinitions[2].Width = GridLength.Auto;
+            }
+            else
+            {
+                g.ColumnDefinitions[2].Width = new GridLength(0);
+            }
+        }
+    }
+}
