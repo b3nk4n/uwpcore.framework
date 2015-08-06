c1ae33c98c72fbbf22224c3481674a5bd8a96b13
bsautermeister <bsautermeister@live.de>
2015-08-06 10:46:47 +0200

    Added base class for data template selection.

---
+++ Framework/UWPCore.Framework/UI/TemplateSelectorBase.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPCore.Framework.UI
+{
+    /// <summary>
+    /// Base template selector class to allow dynamic <see cref="DataTemplate"/> selection.
+    /// </summary>
+    public abstract class TemplateSelectorBase : ContentControl
+    {
+        /// <summary>
+        /// Selects a data template.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="container">The container.</param>
+        /// <returns></returns>
+        public abstract DataTemplate SelectTemplate(object item, DependencyObject container);
+
+        /// <summary>
+        /// The on content changed hook method to react on content changed.
+        /// </summary>
+        /// <param name="oldContent">The old content.</param>
+        /// <param name="newContent">The new content.</param>
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+            ContentTemplate = SelectTemplate(newContent, this);
+        }
+    }
+
+}
