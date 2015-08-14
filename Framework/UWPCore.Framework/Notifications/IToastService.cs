891773c849705ab70245f2c7f45b82dae349d598
bsautermeister <bsautermeister@live.de>
2015-08-14 15:26:41 +0200

    Implemented a toast notification service.

---
+++ Framework/UWPCore.Framework/Notifications/IToastService.cs
@@ -0,0 +1,92 @@
+﻿using Windows.UI.Notifications;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// Interface for toast notification service.
+    /// </summary>
+    public interface IToastService
+    {
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastText1(string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastText02(string title, string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastText03(string title, string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="content1">The first content.</param>
+        /// <param name="content2">The second content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastText04(string title, string content1, string content2, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="image">The image source.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastImageAndText01(string image, string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="image">The image source.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastImageAndText02(string image, string title, string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="image">The image source.</param>
+        /// <param name="content">The content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastImageAndText03(string image, string title, string content, string arg = null);
+
+        /// <summary>
+        /// Creates a toast notification.
+        /// </summary>
+        /// <param name="title">The title.</param>
+        /// <param name="image">The image source.</param>
+        /// <param name="content1">The first content.</param>
+        /// <param name="content2">The second content.</param>
+        /// <param name="arg">The optional launch argument.</param>
+        /// <returns>Returns the toast notification.</returns>
+        ToastNotification CreateToastImageAndText04(string image, string title, string content1, string content2, string arg = null);
+
+        /// <summary>
+        /// Shows the toast notification.
+        /// </summary>
+        /// <param name="toast">The notification to show.</param>
+        void Show(ToastNotification toast);
+    }
+}
---
+++ Framework/UWPCore.Framework/Notifications/ToastService.cs
@@ -0,0 +1,224 @@
+﻿using System.Linq;
+using Windows.UI.Notifications;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// Service class for toast notifications.
+    /// </summary>
+    public class ToastService : IToastService
+    {
+        /// <summary>
+        /// The text node.
+        /// </summary>
+        private const string TEXT_NODE = "text";
+
+        /// <summary>
+        /// The launch attribute to identify the notification when the app is launched via a toast.
+        /// </summary>
+        private const string LAUNCH_ATTRIBUTE = "launch";
+
+        /// <summary>
+        /// The source attribute for images.
+        /// </summary>
+        private const string SRC_ATTRIBUTE = "src";
+
+        public ToastNotification CreateToastText1(string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastText01;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // content
+            var text = xml.CreateTextNode(content);
+            elements[0].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastText02(string title, string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastText02;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[0].AppendChild(text);
+
+            // content
+            text = xml.CreateTextNode(content);
+            elements[1].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastText03(string title, string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastText03;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[0].AppendChild(text);
+
+            // content
+            text = xml.CreateTextNode(content);
+            elements[1].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastText04(string title, string content1, string content2, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastImageAndText04;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[0].AppendChild(text);
+
+            // content1
+            text = xml.CreateTextNode(content1);
+            elements[1].AppendChild(text);
+
+            // content2
+            text = xml.CreateTextNode(content2);
+            elements[2].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastImageAndText01(string image, string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastImageAndText01;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // image
+            elements[0].Attributes
+                .First(x => x.LocalName.Equals(SRC_ATTRIBUTE)).InnerText = image;
+
+            // content
+            var text = xml.CreateTextNode(content);
+            elements[1].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastImageAndText02(string image, string title, string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastImageAndText02;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // image
+            elements[0].Attributes
+                .First(x => x.LocalName.Equals(SRC_ATTRIBUTE)).InnerText = image;
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[1].AppendChild(text);
+
+            // content
+            text = xml.CreateTextNode(content);
+            elements[2].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastImageAndText03(string image, string title, string content, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastImageAndText03;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // image
+            elements[0].Attributes
+                .First(x => x.LocalName.Equals(SRC_ATTRIBUTE)).InnerText = image;
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[1].AppendChild(text);
+
+            // content
+            text = xml.CreateTextNode(content);
+            elements[2].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public ToastNotification CreateToastImageAndText04(string image, string title, string content1, string content2, string arg = null)
+        {
+            // build toast
+            var template = ToastTemplateType.ToastImageAndText04;
+            var xml = ToastNotificationManager.GetTemplateContent(template);
+            var elements = xml.GetElementsByTagName(TEXT_NODE);
+
+            // launch arg
+            if (arg != null)
+                xml.DocumentElement.SetAttribute(LAUNCH_ATTRIBUTE, arg);
+
+            // image
+            elements[0].Attributes
+                .First(x => x.LocalName.Equals(SRC_ATTRIBUTE)).InnerText = image;
+
+            // title
+            var text = xml.CreateTextNode(title);
+            elements[1].AppendChild(text);
+
+            // content1
+            text = xml.CreateTextNode(content1);
+            elements[2].AppendChild(text);
+
+            // content2
+            text = xml.CreateTextNode(content2);
+            elements[3].AppendChild(text);
+
+            return new ToastNotification(xml);
+        }
+
+        public void Show(ToastNotification toast)
+        {
+            var notifier = ToastNotificationManager.CreateToastNotifier();
+            notifier.Show(toast);
+        }
+    }
+}
