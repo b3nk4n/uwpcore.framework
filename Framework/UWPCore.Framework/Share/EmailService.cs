1517d22794ac4ba88b7e7135fa38bc527871a86b
bsautermeister <bsautermeister@live.de>
2015-08-17 14:18:29 +0200

    Added simple email service to send emails.

---
+++ Framework/UWPCore.Framework/Share/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Windows.ApplicationModel.Email;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace UWPCore.Framework.Share
+{
+    /// <summary>
+    /// The email service class that provides basic email functionality.
+    /// </summary>
+    public class EmailService : IEmailService
+    {
+        public async void Show(string[] recipientEmails, string emailBody, StorageFile attachmentFile = null)
+        {
+            var emailMessage = new EmailMessage();
+            emailMessage.Body = emailBody;
+
+            // email recipients
+            foreach (var recipientEmail in recipientEmails)
+            {
+                var email = new EmailRecipient(recipientEmail);
+                emailMessage.To.Add(email);
+            }
+
+            // attachement file
+            if (attachmentFile != null)
+            {
+                var stream = RandomAccessStreamReference.CreateFromFile(attachmentFile);
+                var attachment = new EmailAttachment(attachmentFile.Name, stream);
+                emailMessage.Attachments.Add(attachment);
+            }
+
+            await EmailManager.ShowComposeNewEmailAsync(emailMessage);
+        }
+    }
+}
---
+++ Framework/UWPCore.Framework/Share/IEmailService.cs
@@ -0,0 +1,18 @@
+﻿using Windows.Storage;
+
+namespace UWPCore.Framework.Share
+{
+    /// <summary>
+    /// Interface for email service operations.
+    /// </summary>
+    public interface IEmailService
+    {
+        /// <summary>
+        /// Shows the default application to send an email.
+        /// </summary>
+        /// <param name="recipientEmails">The recipient emails.</param>
+        /// <param name="emailBody">The email body.</param>
+        /// <param name="attachmentFile">The attachement file.</param>
+        void Show(string[] recipientEmails, string emailBody, StorageFile attachmentFile = null);
+    }
+}
