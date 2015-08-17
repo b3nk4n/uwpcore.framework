7d86d9089c0a48fb566004feb064b1bc3f3306f9
bsautermeister <bsautermeister@live.de>
2015-08-17 11:24:07 +0200

    Implemented badge service.

---
+++ Framework/UWPCore.Framework/Notifications/BadgeService.cs
@@ -0,0 +1,59 @@
+﻿using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// The badge service to update the badge icons on live tile or lock screen.
+    /// </summary>
+    public class BadgeService : IBadgeService
+    {
+        /// <summary>
+        /// The badge node selector. 
+        /// </summary>
+        private const string BADGE_SELECTOR = "/badge";
+
+        /// <summary>
+        /// The badge value attribute.
+        /// </summary>
+        private const string VALUE_ATTRIBUTE = "value";
+
+        public BadgeNotification CreateBadgeNumber(int value)
+        {
+            // ensure no negative value
+            if (value < 0)
+                value = 0;
+
+            var xml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+
+            var badgeElement = (XmlElement)xml.SelectSingleNode(BADGE_SELECTOR);
+            badgeElement.SetAttribute(VALUE_ATTRIBUTE, value.ToString());
+
+            return new BadgeNotification(xml);
+        }
+
+        public BadgeNotification CreateBadgeNumber(BadgeGlyphIcon glyph)
+        {
+            var xml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeGlyph);
+
+            // transform first letter to lower case
+            var glyphString = glyph.ToString();
+            glyphString = char.ToLowerInvariant(glyphString[0]) + glyphString.Substring(1);
+
+            var badgeElement = (XmlElement)xml.SelectSingleNode(BADGE_SELECTOR);
+            badgeElement.SetAttribute(VALUE_ATTRIBUTE, glyphString);
+
+            return new BadgeNotification(xml);
+        }
+
+        public BadgeUpdater GetBadgeUpdaterForApplication(string applicationId = null)
+        {
+            return BadgeUpdateManager.CreateBadgeUpdaterForApplication();
+        }
+
+        public BadgeUpdater GetBadgeUpdaterForSecondaryTile(string tileId)
+        {
+            return BadgeUpdateManager.CreateBadgeUpdaterForSecondaryTile(tileId);
+        }
+    }
+}
---
+++ Framework/UWPCore.Framework/Notifications/IBadgeService.cs
@@ -0,0 +1,59 @@
+﻿using Windows.UI.Notifications;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// The badge glyph icons where not all of them are supported in Windows Phone.
+    /// See <see cref="https://msdn.microsoft.com/en-us/library/windows/apps/hh779719.aspx"/> for more information.
+    /// </summary>
+    public enum BadgeGlyphIcon
+    {
+        None,
+        Activity,
+        Alarm,
+        Alert,
+        Attention,
+        Available,
+        Away,
+        Busy,
+        Error,
+        NewMessage,
+        Paused,
+        Playing,
+        Unavailable
+    }
+
+    /// <summary>
+    /// The badge service interface to update the badge icon on the live tile and lock screen.
+    /// </summary>
+    public interface IBadgeService
+    {
+        /// <summary>
+        /// Creates a badge notification.
+        /// </summary>
+        /// <param name="value">The number value.</param>
+        /// <returns>Returns the badge notification.</returns>
+        BadgeNotification CreateBadgeNumber(int value);
+
+        /// <summary>
+        /// Creates a badge notification.
+        /// </summary>
+        /// <param name="glyph">The glyph icon type.</param>
+        /// <returns>Returns the badge notification.</returns>
+        BadgeNotification CreateBadgeNumber(BadgeGlyphIcon glyph);
+
+        /// <summary>
+        /// Gets the application badge updater.
+        /// </summary>
+        /// <param name="applicationId">The application ID that is required for background tasks.</param>
+        /// <returns>Returns the badge updater.</returns>
+        BadgeUpdater GetBadgeUpdaterForApplication(string applicationId = null);
+
+        /// <summary>
+        /// Gets the secondary tile badge updater.
+        /// </summary>
+        /// <param name="tileId">The tile ID.</param>
+        /// <returns>Returns the badge updater.</returns>
+        BadgeUpdater GetBadgeUpdaterForSecondaryTile(string tileId);
+    }
+}
