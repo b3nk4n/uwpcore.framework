59e06127133f14699b674153f9222547bd566928
bsautermeister <bsautermeister@live.de>
2015-08-19 10:02:22 +0200

    Added new launch commands to SettingsLauncher.

--- Framework/UWPCore.Framework/Launcher/SettingsLauncher.cs
+++ Framework/UWPCore.Framework/Launcher/SettingsLauncher.cs
@@ -11,9 +11,94 @@ namespace UWPCore.Framework.Launcher
         /// <summary>
         /// Launches the privacy acounts settings.
         /// </summary>
-        public static async Task LaunchPrivacyAccounts()
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchPrivacyAccounts()
         {
-            await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-accounts"));
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings:privacy-accounts"));
+        }
+
+        /// <summary>
+        /// Lauchnes the airplane mode settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchAirplaneModeAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-airplanemode:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the bluetooth settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchBluetoothAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-bluetooth:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the cellular settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchCellularAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-cellular:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the email accounts settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchEmailAccountsAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-emailandaccounts:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the location settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchLocationAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-location:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the lock screen settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchLockScreenAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-lock:"));
+        }
+
+        /// <summary>
+        /// Lauches the battery saver settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchBatterySaverAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-power:"));
+        }
+
+        /// <summary>
+        /// Lauchnes the screen rotation settings.
+        /// </summary>
+        /// <remarks>
+        /// Requires the GDR3 update.
+        /// </remarks>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchScreenRotationAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-screenrotation:"));
+        }
+
+        /// <summary>
+        /// Lauches the wifi settings.
+        /// </summary>
+        /// <returns>Returns true if successful, else false.</returns>
+        public static async Task<bool> LaunchWifiAsync()
+        {
+            return await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-settings-wifi:"));
         }
     }
 }
