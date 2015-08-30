28b85d11883a46fcc8d6b53f58c51ced80a5f1b1
bsautermeister <bsautermeister@live.de>
2015-08-30 20:30:38 +0200

    Added missing files of the badge factory.

---
+++ Framework/UWPCore.Framework/Notifications/BadgeFactory.cs
@@ -0,0 +1,52 @@
+﻿using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using UWPCore.Framework.Common;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// Inferface for factories to create badge notifications based on a template.
+    /// </summary>
+    /// <remarks>
+    /// For further information about the templates refer to <see cref="https://msdn.microsoft.com/en-us/library/windows/apps/hh779719.aspx"/>.
+    /// </remarks>
+    public class BadgeFactory : IBadgeFactory
+    {
+        /// <summary>
+        /// The badge node selector. 
+        /// </summary>
+        private const string BADGE_SELECTOR = "/badge";
+
+        /// <summary>
+        /// The badge value attribute.
+        /// </summary>
+        private const string VALUE_ATTRIBUTE = "value";
+
+        public BadgeNotification CreateBadgeNumber(int value)
+        {
+            // ensure no negative value
+            if (value < 0)
+                value = 0;
+
+            var xml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeNumber);
+
+            var badgeElement = (XmlElement)xml.SelectSingleNode(BADGE_SELECTOR);
+            badgeElement.SetAttribute(VALUE_ATTRIBUTE, value.ToString());
+
+            return new BadgeNotification(xml);
+        }
+
+        public BadgeNotification CreateBadgeNumber(BadgeGlyphIcon glyph)
+        {
+            var xml = BadgeUpdateManager.GetTemplateContent(BadgeTemplateType.BadgeGlyph);
+
+            // transform first letter to lower case
+            var glyphString = glyph.ToString().FirstLetterToLower();
+
+            var badgeElement = (XmlElement)xml.SelectSingleNode(BADGE_SELECTOR);
+            badgeElement.SetAttribute(VALUE_ATTRIBUTE, glyphString);
+
+            return new BadgeNotification(xml);
+        }
+    }
+}
---
+++ Framework/UWPCore.Framework/Notifications/IBadgeFactory.cs
@@ -0,0 +1,50 @@
+﻿using Windows.UI.Notifications;
+
+namespace UWPCore.Framework.Notifications
+{
+    /// <summary>
+    /// The badge glyph icons where not all of them are supported in Windows Phone.
+    /// </summary>
+    /// <remarks>
+    /// See <see cref="https://msdn.microsoft.com/en-us/library/windows/apps/hh779719.aspx"/> for more information.
+    /// </remarks>
+    public enum BadgeGlyphIcon
+    {
+        None,
+        Activity,
+        Alarm,
+        Alert,
+        Attention,
+        Available,
+        Away,
+        Busy,
+        Error,
+        NewMessage,
+        Paused,
+        Playing,
+        Unavailable
+    }
+
+    /// <summary>
+    /// Inferface for factories to create badge notifications based on a template.
+    /// </summary>
+    /// <remarks>
+    /// For further information about the templates refer to <see cref="https://msdn.microsoft.com/en-us/library/windows/apps/hh779719.aspx"/>.
+    /// </remarks>
+    public interface IBadgeFactory
+    {
+        /// <summary>
+        /// Creates a badge notification.
+        /// </summary>
+        /// <param name="value">The number value.</param>
+        /// <returns>Returns the badge notification.</returns>
+        BadgeNotification CreateBadgeNumber(int value);
+
+        /// <summary>
+        /// Creates a badge notification.
+        /// </summary>
+        /// <param name="glyph">The glyph icon type.</param>
+        /// <returns>Returns the badge notification.</returns>
+        BadgeNotification CreateBadgeNumber(BadgeGlyphIcon glyph);
+    }
+}
