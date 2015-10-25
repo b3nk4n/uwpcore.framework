d505150989c9949c874f48d94e9133494572c9a5
Patrick Mutter <patrick.mutter@mperia.de>
2015-10-25 17:24:09 +0100

    Add creation of demo data.

--- Demo/UWPCore.Demo/Views/OneDrivePage.xaml.cs
+++ Demo/UWPCore.Demo/Views/OneDrivePage.xaml.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using UWPCore.Demo.ViewModels;
+using UWPCore.Framework.Storage;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage.Pickers.Provider;
@@ -31,6 +32,28 @@ namespace UWPCore.Demo.Views
         public OneDrivePage()
         {
             this.InitializeComponent();
+
+            this.Loaded += OneDrivePage_Loaded;
+        }
+
+        private async void OneDrivePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // create example data if no exist
+            var localStorageService = new LocalStorageService();
+            var folder = localStorageService.GetFolderAsync("Data");            
+            if(folder == null)
+            {
+                await localStorageService.CreateOrGetFolderAsync("Data");
+
+                var file = await localStorageService.CreateOrGetFileAsync("loremipsum.txt");
+                using (var stream = await file.OpenStreamForWriteAsync())
+                {
+                    using (var sw = new StreamWriter(stream))
+                    {
+                        await sw.WriteAsync("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.");
+                    }
+                }                
+            }
         }
     }
 }
