fcd92b3b5acd96315d235e1e8f38f956bb1bc057
bsautermeister <bsautermeister@live.de>
2015-10-27 13:07:54 +0100

    Added repository pattern interfaces and base implementation.

---
+++ Framework/UWPCore.Framework/Data/IRepositoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UWPCore.Framework.Data
+{
+    /// <summary>
+    /// Interface for model items in the <see cref="IRepository{TEntity, TKey}"/>.
+    /// Use <see cref="Nullable{T}"/> wherever possible, which is used by the prototype-update mechanism.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the ID.</typeparam>
+    public interface IRepositoryItem<TKey>
+    {
+        /// <summary>
+        /// Gets the ID.
+        /// </summary>
+        TKey Id { get; }
+    }
+}
