88d38b5c1e271fcb4f3d62c27111e4cab7e2952c
Patrick Mutter <patrick.mutter@mperia.de>
2015-10-31 02:20:05 +0100

    Update View of disco tile.

--- Demo/UWPCore.Demo.Tasks/DiscoTileBackgroundTask.cs
+++ Demo/UWPCore.Demo.Tasks/DiscoTileBackgroundTask.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using UWPCore.Framework.Storage;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Background;
 using Windows.Data.Xml.Dom;
 using Windows.Foundation;
-using Windows.Globalization.DateTimeFormatting;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Notifications;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
@@ -22,13 +17,35 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace UWPCore.Demo.Tasks
 {
-    //https://code.msdn.microsoft.com/windowsapps/Updating-a-tile-from-a-e0e492df/view/SourceCode#content
-    public sealed class DiscoTileBackgroundTask : XamlRenderingBackgroundTask
+    // https://code.msdn.microsoft.com/windowsapps/Updating-a-tile-from-a-e0e492df/view/SourceCode#content
+    // TODO: Crash if using IBackgroundTask because of different task contexts.
+    // TODO: Sometimes Tile disappears
+    public sealed class DiscoTileBackgroundTask : XamlRenderingBackgroundTask //, IBackgroundTask
     {
-        // RenderTargetBitmap Pixel Data
-        int pixelWidth;
-        int pixelHeight;
-        byte[] pixelData;
+        #region Fields
+
+        ILocalStorageService localStorageService;
+        Random random;
+
+        #endregion
+
+        #region Constructors
+
+        public DiscoTileBackgroundTask()
+        {
+            localStorageService = new LocalStorageService();
+            random = new Random();
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            System.Diagnostics.Debug.WriteLine("Run called!");
+            OnRun(taskInstance);
+        }
 
         // When the trigger for which this backgroudn task is fired, the OnRun method will be called. 
         // Since this background task is a XamlRenderingBackgroundTask, we can render XAML and use that 
@@ -36,19 +53,25 @@ namespace UWPCore.Demo.Tasks
         protected override async void OnRun(IBackgroundTaskInstance taskInstance)
         {
             var deferral = taskInstance.GetDeferral();
-
-            await GenerateHighResTileImageUpdateAsync("discoTile.xml", "discoTile.png", new Size(150, 150));
-
-            UpdateTile("discoTile.png");
+            await CreateAndUpdateDiscoTileAsync("discoTile.xml", "Tiles/discoTile.png", new Size(150, 150));
 
             deferral.Complete();
         }
 
-        async Task GenerateHighResTileImageUpdateAsync(string inputMarkupFilename, string outputImageFilename, Size size)
+        /// <summary>
+        /// Creates the DiscoTile image and update the tile.
+        /// </summary>
+        /// <param name="inputMarkupFilename"></param>
+        /// <param name="outputImageFilename"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        private async Task CreateAndUpdateDiscoTileAsync(string inputMarkupFilename, string outputImageFilename, Size size)
         {
+            // Get Tile markup
             var assetsFolder = await Package.Current.InstalledLocation.GetFolderAsync("Assets");
             var markupStorageFile = await assetsFolder.GetFileAsync(inputMarkupFilename);
 
+            // Update Elements
             var stream = await markupStorageFile.OpenReadAsync();
             string markupContent;
             using (var sr = new StreamReader(stream.AsStreamForRead()))
@@ -64,9 +87,9 @@ namespace UWPCore.Demo.Tasks
             // Transparency is also supported in tiles. 
             // To make the tile transparent, so that the start screen background picture that your user selected shows through the
             // tile, set Background color property in the Package.appxmanifest to "transparent".
-            border.Background = GetRandomBackgroundBrush();
+            border.Background = new SolidColorBrush(Windows.UI.ColorHelper.FromArgb(255, 0, (byte)(random.Next() % 255), (byte)(random.Next() % 255)));
 
-            // Change the text of the Timestamp TextBlock.
+            // Change the text of the Title TextBlock.
             Grid grid = (Grid)border.Child;
             TextBlock titleText = (TextBlock)grid.FindName("Title");
             titleText.Text = string.Format("{0:hh:mm}", DateTime.Now);
@@ -76,41 +99,26 @@ namespace UWPCore.Demo.Tasks
             var bitmapImage = new BitmapImage() { CreateOptions = BitmapCreateOptions.None };
             bitmapImage.UriSource = new Uri("ms-appx:///Assets/disco-ball.png");
             logoImage.Source = bitmapImage;
-            
-            // Render the XAML, in this example a Border and its content, to a bitmap and save the bitmap to a file. 
-            await RenderAndSaveToFileAsync(border, outputImageFilename, (int)size.Width, (int)size.Height);
-        }
 
-        async Task RenderAndSaveToFileAsync(UIElement uiElement, string outputImageFilename, int width, int height)
-        {
+            // Render DiscoTile
             RenderTargetBitmap rtb = new RenderTargetBitmap();
-            await rtb.RenderAsync(uiElement, width, height);
-            pixelWidth = rtb.PixelWidth;
-            pixelHeight = rtb.PixelHeight;
+            await rtb.RenderAsync(border, (int)size.Width, (int)size.Height);
             var buffer = await rtb.GetPixelsAsync();
 
-            StorePixelsFromBuffer(buffer);
-            await WriteBufferToFile(outputImageFilename);
-        }
-
-        void StorePixelsFromBuffer(IBuffer buffer)
-        {
+            // Read buffer into byte array.
             DataReader dataReader = DataReader.FromBuffer(buffer);
             var data = new byte[buffer.Length];
             dataReader.ReadBytes(data);
 
-            pixelData = data;
-        }
-
-        async Task WriteBufferToFile(string outputImageFilename)
-        {
-            var outputStorageFile = await ApplicationData.Current.LocalFolder.CreateFileAsync(outputImageFilename, Windows.Storage.CreationCollisionOption.ReplaceExisting);            
+            // Save DiscoTile to local storage
+            var outputStorageFile = await localStorageService.CreateOrGetFileAsync(outputImageFilename);
             var outputStream = await outputStorageFile.OpenAsync(FileAccessMode.ReadWrite);
             var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, outputStream);
-            encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied, (uint)pixelWidth, (uint)pixelHeight, 96, 96, pixelData);
+            encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied, (uint)size.Width, (uint)size.Height, 96, 96, data);
             await encoder.FlushAsync();
 
-            pixelData = null;
+            // Update Tile with image.
+            UpdateTile(outputImageFilename);
         }
 
         // Send a tile notification with the new tile payload. 
@@ -120,44 +128,12 @@ namespace UWPCore.Demo.Tasks
             tileUpdater.Clear();
             var tileTemplate = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Image);
             var tileImageAttributes = tileTemplate.GetElementsByTagName("image");
-            ((XmlElement)tileImageAttributes[0]).SetAttribute("src", "ms-appdata:///local/discoTile.png");
+            ((XmlElement)tileImageAttributes[0]).SetAttribute("src", string.Format("ms-appdata:///local/{0}", tileUpdateImagePath));
 
-            //XmlElement tmp = tileImageAttributes.Item(0) as XmlElement;
-            //tmp.SetAttribute("src", "UpdatedLiveTile.png");
-
-
-            ////((XmlElement)tileImageAttributes.Item(0)).SetAttribute("src", "ms-appdata:///local/updatedTile.png");
             var notification = new TileNotification(tileTemplate);
             tileUpdater.Update(notification);
-
-            //XmlDocument mediumTileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Image);
-            //XmlNodeList mediumTileImageAttributes = mediumTileXml.GetElementsByTagName("image");
-            //((XmlElement)mediumTileImageAttributes[0]).SetAttribute("src", "ms-appdata:///local/" + fileNameMedium);
-            //((XmlElement)mediumTileImageAttributes[0]).SetAttribute("alt", "AltString");
-
         }
 
-        // Helper method to get the current time to be used when updating the tile in this sample.
-        string GetCurrentTime()
-        {
-            DateTime datetime = DateTime.Now;
-            DateTimeFormatter datetimeFormat = DateTimeFormatter.ShortTime;
-            return datetimeFormat.Format(datetime);
-        }
-
-
-        // Helper method to get a random background color to be used when updating the tile in this sample.
-        Brush GetRandomBackgroundBrush()
-        {
-            // Seed our random number generator.
-            Random rand = new Random();
-            return new SolidColorBrush(Windows.UI.ColorHelper.FromArgb(255, 0, (byte)(rand.Next() % 255), (byte)(rand.Next() % 255)));
-        }
-
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-            System.Diagnostics.Debug.WriteLine("Run called!");
-            OnRun(taskInstance);
-        }
+        #endregion
     }
 }
