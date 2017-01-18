ca7d0fa46d0fe9002dc0e4ce02650b27827c1cd0
Patrick Mutter <patrick.mutter@mperia.de>
2017-01-18 22:46:06 +0100

    Update zip archive. Solved issue #26 and isuue #24.

--- Framework/UWPCore.Framework/Data/ICompressionService.cs
+++ Framework/UWPCore.Framework/Data/ICompressionService.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace UWPCore.Framework.Data
 {
@@ -8,11 +10,28 @@ namespace UWPCore.Framework.Data
     public interface ICompressionService
     {
         /// <summary>
+        /// Compress folder async to file.
+        /// </summary>
+        /// <param name="folder">Folder to compress. Only folder content will be zipped.</param>
+        /// <param name="file">File to compress.</param>
+        /// <returns></returns>
+        Task<bool> CompressAsync(IStorageFolder folder, IStorageFile file);
+
+        /// <summary>
+        /// Uncompress file into folder.
+        /// </summary>
+        /// <param name="file">Target file to uncompress.</param>
+        /// <param name="folder">Folder data should be uncompressed in.</param>
+        /// <returns></returns>
+        Task<bool> UncompressAsync(IStorageFile file, IStorageFolder folder);
+
+        /// <summary>
         /// Compresses and create an archive of a folder.
         /// </summary>
         /// <param name="path">Path to folder.</param>
         /// <param name="name">Alternative compressed folder name.</param>
         /// <returns>Returns task to await.</returns>
+        [Obsolete]
         Task CompressAsync(string path, string name = null);
 
         /// <summary>
@@ -21,6 +40,7 @@ namespace UWPCore.Framework.Data
         /// <param name="path">Path to zip archive.</param>
         /// <param name="name">Alternative uncompressed folder name.</param>
         /// <returns>Returns task to await.</returns>
+        [Obsolete]
         Task UncompressAsync(string path, string name = null);
     }
 }
